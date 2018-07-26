b3bab83cff0dc2548ee8a1d00aa22b8bc2b0d6f3
lucypeony <lucypeony@hotmail.com>
2018-07-26 19:27:18 +0800

    dp failed

--- prefix.cpp
+++ prefix.cpp
@@ -7,6 +7,10 @@ PROB:prefix
 Longest Prefix
 这一题普通的方法没用，要用dp
 在我提供了好多次，前四个测试点都过了。第五个测试点过不了的时候。我才明白。
+
+动态转移方程：
+设dp[i]为到i为止的子串组成的最长长度
+dp[i]= max{dp[i-1],  dp[i-length(p[j]) + length(p[j])}
 */
 #include<fstream>
 #include<iostream>
@@ -29,7 +33,27 @@ int main() {
 		seq += s;
 	}
 
-	
+	//dp[i]= max{dp[i-1],  dp[i-length(p[j]) + length(p[j])}
+	int dp[200010] = { 0 };
+	int l = seq.size();
+	for (int i = 1; i <= l; i++) {
+		dp[i] = dp[i - 1];
+		for (auto p : primitives) {
+			int plen = p.size();
+			if (plen <= i) {
+				string t = seq.substr(i - plen, plen);
+				if (t == p) {
+					if (dp[i] < dp[i - plen] + plen)
+						dp[i] = dp[i - plen] + plen;
+				}
+					
+			}
+		}
+	}
+
+	for (int i = 0; i < l; i++)
+		if (ans < dp[i])
+			ans = dp[i];
 
 	cout << ans << endl;
 
